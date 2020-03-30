0d4ff84570d47d7e2129bb4dfd61ebc6c584f61c
rahulg510 <rahulg510@live.com>
2020-03-30 04:15:06 -0700

    Implement GET,POST,DELETE HTTP methods

--- routes/api.js
+++ routes/api.js
@@ -9,45 +9,82 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
+
 var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
-//Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function (app,db) {
 
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      var title = req.body.title;
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
-    });
+    app.route('/api/books')
+      .get(function (req, res){
+        db.find().toArray((err,docs) =>{
+          let arr = [];
+          for(var i = 0; i < docs.length; i++){
+            let book = {
+              title: docs[i]['title'],
+              _id: docs[i]['_id'],
+              commentCount: docs[i]['comments'] !=null ? docs[i]['comments'].length : 0
+            }
+            arr.push(book);
+          }
+          res.send(arr);
+       })
+      })
+      
+      .post(function (req, res){
+        var book= {
+          title: req.body.title,
+          comments: []
+        }
+        db.insert(book,function(err,doc){
+          if(err) res.send('error' + err);
+          else{
+            res.send(doc.ops[0]);
+          }
+        })
+        
+      })
+      
+      .delete(function(req, res){
+        db.remove({},function(e,d){
+          if(e) return res.send("Unable to delete all the books");
+          res.send('All books deleted');
+        });
+      });
 
+    app.route('/api/books/:id')
+      .get(function (req, res){
+        var bookid = req.params.id;
+        db.findOne(ObjectId(bookid),(err,doc)=>{
+          if(err) return res.send("No book exists");
+          else{
+            return res.send(doc);
+          }
+        })
+        //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      })
+      
+      .post(function(req, res){
+        var bookid = req.params.id;
+        var comment = req.body.comment;
+        if(comment != null){
+        db.findOneAndUpdate({_id: ObjectId(bookid)},{$push: {comments: comment}},(err,doc)=>{
+         if (err) return res.send("No book exists");
+         else{
+          return res.send(doc['value']);
+         }
+       })
+      }
+        else return res.send("Input a comment");
+      })
+      
+      .delete(function(req, res){
+        var bookid = req.params.id;
+        db.remove({_id: ObjectId(bookid)},function(err,status){
+          if(err) return res.send("Unable to delete the book" + err);
+          return res.send("Book deleted");
 
+        })
+      });
 
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      var bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-    })
-    
-    .post(function(req, res){
-      var bookid = req.params.id;
-      var comment = req.body.comment;
-      //json res format same as .get
-    })
-    
-    .delete(function(req, res){
-      var bookid = req.params.id;
-      //if successful response will be 'delete successful'
-    });
-  
 };
+
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
+var helmet      = require('helmet');
+var MongoClient = require('mongodb').MongoClient;
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -16,7 +18,28 @@ app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy('PHP 4.2.0'));
+const MONGODB_CONNECTION_STRING = process.env.DB;
 
+MongoClient.connect(MONGODB_CONNECTION_STRING,function(err,database){
+  if(err) return console.error(err);
+
+  database.db('library').createCollection('books',{
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["title"],
+        properties:
+        {
+          title: {
+            bsonType: "string"
+          }
+        }
+      }
+    }},function(err,db){
+      if(err) return console.log(err);
+      console.log("Database connection established");
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
@@ -27,7 +50,7 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
+apiRoutes(app,db);  
     
 //404 Not Found Middleware
 app.use(function(req, res, next) {
@@ -38,7 +61,7 @@ app.use(function(req, res, next) {
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+  console.log("Listening on port " + 3000);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
@@ -54,3 +77,5 @@ app.listen(process.env.PORT || 3000, function () {
 });
 
 module.exports = app; //for unit/functional testing
+    })
+})
